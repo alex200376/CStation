17e73af10e52c7f90c74ff07fa4f94c441ba304d
Alex Tsang <anthorytsang@gmail.com>
2023-02-23 19:58:14 +0800

    modified:   assets/css/style.css 	modified:   assets/js/script.js modified:   pc_create.html 	modified:   product.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var sum = 0;
+
     $('.add_to_cart').click(function() {
         var productCard = $(this).parent();
         var position = productCard.offset();
@@ -13,7 +14,6 @@ $(document).ready(function() {
         $(cart).css({ 'top': position.top + 'px', "left": position.left + 'px' }).fadeIn("slow").addClass('moveToCart');
         setTimeout(function() { $("body").addClass("MakeFloatingCart"); }, 800);
         setTimeout(function() {
-
             sum += productprice;
             $('div.floating-cart').remove();
             $("body").removeClass("MakeFloatingCart");
@@ -27,8 +27,7 @@ $(document).ready(function() {
             localStorage.totalprice = $('#total').html();
             localStorage.cart = $('#cart').html();
             $("#cart .cart-item").last()
-
-            .addClass("flash")
+                .addClass("flash")
                 .find(".delete-item").click(function() {
                     $(this).parent().fadeOut(300, function() {
                         var x = $(this).find('.cs').get(0).innerHTML;
